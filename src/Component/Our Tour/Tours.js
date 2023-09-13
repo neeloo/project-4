e5690cfc3063e9dcf451233ebc5aa0d804915310
Nilu patel <nilusingh945164@gmail.com>
2023-09-13 09:29:48 +0530

    p2

--- src/Component/Our Tour/Tours.js
+++ src/Component/Our Tour/Tours.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+const url='https://newsapi.org/v2/top-headlines?apiKey=e7c9a3e803474bc19df75800c865e402&q=cricket'
 
 const Tours = () => {
+  const[loading,setloading]=useState(true);
   return (
     <>
     <h1>our tour project</h1>
