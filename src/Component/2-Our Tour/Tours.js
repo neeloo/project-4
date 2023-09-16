bf5a2c29a07f4b3831e1f9c7d5ae6a72753118a4
Nilu patel <nilusingh945164@gmail.com>
2023-09-16 07:29:41 +0530

    p2

--- src/Component/2-Our Tour/Tours.js
+++ src/Component/2-Our Tour/Tours.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Tour from './Tour';
 const Tours = ({ tours, removeTour }) => {
   return (
+    
     <section>
       <div className="title">
         <h2>Our Tours</h2>
