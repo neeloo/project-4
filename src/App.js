17d7b1074f8b59cfb28d4061916daaf404088381
Nilu patel <nilusingh945164@gmail.com>
2023-09-27 19:57:32 +0530

    p6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //import AppJ from './Component/7-pokaman App/AppJ';
-//import AppT from './Component/6-tabs/AppT';
+import AppT from './Component/6-tabs/AppT';
 // import AppQ from './Component/4-Question project/AppQ';
  //import AppR from './Component/3-Review Project/AppR';
 //import AppA from './5-meanu App/AppA';
@@ -28,7 +28,7 @@ function App() {
      {/* <AppA/> */}
 
      {/* 6-tabs */}
-     {/* <AppT/> */}
+     <AppT/>
 
      {/* 7-poka ma */}
      {/* <AppJ/> */}
