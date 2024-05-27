f899403481c01b8e1dc65bc31ca582d2fbaf1e88
Nilu patel <nilusingh945164@gmail.com>
2024-05-27 22:29:19 +0530

    ght

--- project-5/src/App.js
+++ project-5/src/App.js
@@ -33,21 +33,21 @@ function App() {
   const [courses, setCourses] = useState([
     {
       id: 1,
-      name: 'GFG T-shirt',
+      name: ' T-shirt',
       price: 499,
       image:
         'https://media.geeksforgeeks.org/wp-content/uploads/20230823165506/gfg1.png'
     },
     {
       id: 2,
-      name: 'GFG Bag',
+      name: ' Bag',
       price: 699,
       image:
         'https://media.geeksforgeeks.org/wp-content/uploads/20230823165553/gfg2.jpg'
     },
     {
       id: 3,
-      name: 'GFG Hoodie',
+      name: ' Hoodie',
       price: 799,
       image:
         'https://media.geeksforgeeks.org/wp-content/uploads/20230823165623/gfg3.jpg'
